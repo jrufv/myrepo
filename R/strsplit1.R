0d08c0523b1aa6975fece6dc964b05c4ff4b7bf1
jrufv <jordiruf@gmail.com>
2025-04-04 13:51:39 +0200

    Commit

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,14 @@
+#' Separa una cadena
+#'
+#' @param x Un vector de carácteres con un elemento.
+#' @param split ¿Por donde separamos?.
+#'
+#' @returns Un vector de carácteres.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
